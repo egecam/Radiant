f517fbb7ff3bbe2c929fdd8a9ebe85ee42286923
Ege <egecam000@gmail.com>
2024-01-13 14:32:26 +0300

    Initial Commit

---
+++ Radiant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Radiant
+//
+//  Created by Ege Çam on 13.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Radiant/RadiantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadiantApp.swift
+//  Radiant
+//
+//  Created by Ege Çam on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RadiantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
